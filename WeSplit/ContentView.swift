ba9fa0410c7d10821b12e947ff20a7daa1f1483d
Xun Ruan <galaxymacos@gmail.com>
2021-07-02 23:57:06 +0800

    Understand ContentView.swift

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -7,13 +7,18 @@
 
 import SwiftUI
 
+// View is a basic protocol for anything you want to draw on screen
+// the only limitation of View is to have a body property that returns some View
 struct ContentView: View {
+    // {} return something that confirm to View, and "some" means you can only return one kind of View
     var body: some View {
+        // Text view can automatically wrapped to multiple lines
         Text("Hello, world!")
             .padding()
     }
 }
 
+// Only affect the look but not the actual app
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
