71e583f2d5495d1569a5295253dffe8fa293ebab
Xun Ruan <galaxymacos@gmail.com>
2021-07-03 08:50:02 +0800

    Creating views in a loop

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -9,14 +9,25 @@ import SwiftUI
 
 
 struct ContentView: View {
-        @State var name: String = ""
-        var body: some View {
-            Form{
-                // $ = it should read the value of the property but also write it back as any changes happen.
-                TextField("TextField", text: $name)
-                Text("Your name is \(name)")
+    let students = ["Harry", "Hermione", "Ron"]
+    @State private var selectedStudent = 0
+    var body: some View {
+        VStack{
+            // the description will not show on screen, but it is useful for screen reader
+            Picker("Who is your fav student?", selection: $selectedStudent){
+                // 1st way to write ForEach
+                ForEach (0..<students.count) {
+                    Text(students[$0])
+                }
+                // 2nd way to write ForEach
+//                ForEach(0..<students.count){
+//                    number in
+//                    Text(students[number])
+//                }
             }
+            Text("The selected student is \(students[selectedStudent])")
         }
+    }
 }
 
 
